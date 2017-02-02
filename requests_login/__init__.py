65547c80ad9a7dce471189940a651c6cb58db22e
jxltom <jxltom@gmail.com>
2017-02-02 10:18:01 +0800

    Add setup

---
+++ requests_login/__init__.py
@@ -0,0 +1,3 @@
+__title__ = 'requests-login'
+__version__ = '0.1.0'
+__author__ = 'jxltom'
---
+++ setup.py
@@ -0,0 +1,22 @@
+import re
+from setuptools import setup, find_packages
+
+_version_re = r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]'
+
+with open('requests/__init__.py', 'r') as f:
+    version = re.search(_version_re, f.read(), re.MULTILINE).group(1)
+
+setup(
+    name='requests-login',
+    version=version,
+    packages=find_packages(),
+    install_requires=['requests>=2.11.0'],
+
+    author='jxltom',
+    author_email='jxltom@gmail.com',
+    license='MIT',
+    keywords="hello world example examples",
+    url='https://github.com/jxltom/requests-login/',
+    description='Simple way for logging in any forums or websites '
+                'based on requests',
+)
